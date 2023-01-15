35f07a388037b741015d52bbef3359733e2c23c4
raju.mangoking <raju.mangoking@gmail.com>
2023-01-15 14:31:17 +0000

    pushing simple

---
+++ simple/src/greet/Welcome.java
@@ -0,0 +1,14 @@
+package greet;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hiiiiiiiiiiiiiiiii everyone!!!!!!!!!!!!!!!");
+		
+		
+	}
+
+}
